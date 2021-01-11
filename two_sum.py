3db68c1d098727ff8bc8ef5405a73a156f5e233e
jtreeves <jackson.t.reeves@gmail.com>
2021-01-11 16:39:13 -0500

    [two_sum] Add a quadratic and linear option

--- two_sum.py
+++ two_sum.py
@@ -1,13 +1,43 @@
-def two_sum(number_list, sum):
-# TK
+def two_sum1(number_list, sum):
+    for i in range(len(number_list)):
+        for j in range(len(number_list)):
+            if (i != j) and (number_list[i] + number_list[j] == sum):
+                return [i, j]
 
-print(two_sum([11, 2, 7, 15], 9))
+# QUADRATIC TIME COMPLEXITY
+
+print(two_sum1([11, 2, 7, 15], 9))
 # => [1,2]
-print(two_sum([3, 2, 9, 11], 5))
+print(two_sum1([3, 2, 9, 11], 5))
 # => [0,1]
-print(two_sum([17, 8, 22, 1, 4, 6], 5))
+print(two_sum1([17, 8, 22, 1, 4, 6], 5))
 # => [3,4]
-print(two_sum([1, 8, 22, 3, 4, 5], 4))
+print(two_sum1([1, 8, 22, 3, 4, 5], 4))
 # => [0,3]
-print(two_sum([1, 2, 3, 4, 5, 6], 40))
+print(two_sum1([1, 2, 3, 4, 5, 6], 40))
+# => []
+
+# ALTERNATIVE ---------------------------------------------------
+
+def two_sum2(number_list, sum):
+    intermediary = {}
+    for i in range(len(number_list)):
+        diff = str(sum - number_list[i])
+        intermediary[diff] = i
+    for j in range(len(number_list)):
+        value = str(number_list[j])
+        if intermediary.has_key(value) and j != intermediary[value]:
+            return [j, intermediary[value]]
+
+# LINEAR TIME COMPLEXITY
+
+print(two_sum2([11, 2, 7, 15], 9))
+# => [1,2]
+print(two_sum2([3, 2, 9, 11], 5))
+# => [0,1]
+print(two_sum2([17, 8, 22, 1, 4, 6], 5))
+# => [3,4]
+print(two_sum2([1, 8, 22, 3, 4, 5], 4))
+# => [0,3]
+print(two_sum2([1, 2, 3, 4, 5, 6], 40))
 # => []
